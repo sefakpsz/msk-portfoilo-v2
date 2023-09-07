b2cc464e9c84216fa3e9303654bfa810451b5461
Fatih YILDIZ <fatih.yildiz@primetech.com>
2023-09-07 11:13:38 +0300

    header responsive

--- src/components/layout/header/index.tsx
+++ src/components/layout/header/index.tsx
@@ -1,14 +1,21 @@
 import React from "react";
 import { MainNav } from "./main-nav";
 import Social from "./social";
+import { Button } from "@/components/ui/button";
+import { Menu } from "lucide-react";
 
 function Header() {
   return (
     <div className="border-b fixed top-0 inset-x-0">
-      <div className="flex h-16 items-center px-4 max-w-7xl mx-auto">
+      <div className="flex justify-between h-16 items-center px-4 max-w-7xl mx-auto">
         <h1 className="font-extrabold mr-5">Sefakpsz.</h1>
-        <MainNav className="mx-6" />
-        <Social />
+        <div className="w-full items-center hidden sm:flex">
+          <MainNav className="mx-6" />
+          <Social />
+        </div>
+        <Button variant="ghost" className="px-2 sm:hidden">
+            <Menu height={25} strokeWidth={1.5} />
+        </Button>
       </div>
     </div>
   );
