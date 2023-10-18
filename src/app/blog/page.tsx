a8bbd8a16e13cbd68313780335524b8444305ddc
sefakpsz <m.sefa06@hotmail.com>
2023-10-18 19:28:35 +0300

    feat: grid added

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -2,16 +2,18 @@ import Blog from '@/components/section/Blog'
 
 export default function Home() {
     return (
-        <main className="flex min-h-screen items-center justify-between p-24 w-[%60]">
-            {
-                [...new Array(5)].map((item, index) => {
-                    if (index === 0) {
-                        return <Blog key={index} title='Blog Title' image='Blog image' category='Blog Category' summary='Blog Summary'
-                            className='w-full h-[600px]' />
-                    }
-                    return <Blog key={index} title='Blog Title' image='Blog image' category='Blog Category' summary='Blog Summary' className='' />
-                })
-            }
+        <main className="grid grid-cols-4 gap-4 p-32 w-full">
+            {[...new Array(6)].map((item, index) => (
+
+                <Blog
+                    key={index}
+                    title='Blog Title'
+                    image='Blog image'
+                    category='Blog Category'
+                    summary='Blog Summary'
+                    className={`${index !== 0 ? 'col-span-1 row-span-1 h-[290px]' : 'col-span-2 row-span-2 h-[600px]'}`}
+                />
+            ))}
         </main>
     )
 }
--- src/components/section/Blog/index.tsx
+++ src/components/section/Blog/index.tsx
@@ -8,32 +8,34 @@ import {
 import { Avatar, AvatarFallback, AvatarImage } from "../../ui/avatar"
 import { twMerge } from "tailwind-merge"
 import { BlogProps } from "./types"
-
+import Link from "next/link"
 
 const Blog: React.FC<BlogProps> = ({ className, image, category, title, summary }) => {
 
     return (
-        <Card className={twMerge('w-full max-w-7xl space-y-6 mx-auto p-12 m-2', className)}>
-            <p>{image}</p>
-            <div className="flex items-center gap-3">
-                <Avatar className="w-8 h-8">
-                    <AvatarImage src="https://github.com/sefakpsz.png" />
-                    <AvatarFallback className="font-bold text-xl">SF</AvatarFallback>
-                </Avatar>
-                <div className="flex-col items-center">
-                    <p className="font-medium font-sans text-xs">Muhammet Sefa Kapısız</p>
+        <Link href="/">
+            <Card className={twMerge('w-full max-w-7xl space-y-6 mx-auto p-8 m-[1px] hover:cursor-pointer hover:bg-slate-50', className)}>
+                <p>{image}</p>
+                <div className="flex items-center gap-3">
+                    <Avatar className="w-8 h-8">
+                        <AvatarImage src="https://github.com/sefakpsz.png" />
+                        <AvatarFallback className="font-bold text-xl">SF</AvatarFallback>
+                    </Avatar>
+                    <div className="flex-col items-center">
+                        <p className="font-medium font-sans text-xs">Muhammet Sefa Kapısız</p>
+                    </div>
                 </div>
-            </div>
-            <CardTitle>{category}</CardTitle>
-            <CardTitle>{title}</CardTitle>
-            <CardTitle>{summary}</CardTitle>
+                <CardTitle>{category}</CardTitle>
+                <CardTitle>{title}</CardTitle>
+                <CardTitle>{summary}</CardTitle>
 
-            {/* <Button>Comment</Button>
+                {/* <Button>Comment</Button>
 
-            <p>Number of Comments - {numberOfComment}</p>
+<p>Number of Comments - {numberOfComment}</p>
 
-            <Button>Like</Button> */}
-        </Card>
+<Button>Like</Button> */}
+            </Card>
+        </Link>
     )
 }
 
