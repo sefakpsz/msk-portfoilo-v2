9c4031665139a56bd6d439818a356ae38196e875
sefakpsz <m.sefa06@hotmail.com>
2023-11-15 00:55:51 +0300

    fix: next/link fixed

--- src/components/layout/header/index.tsx
+++ src/components/layout/header/index.tsx
@@ -5,13 +5,11 @@ import { Button } from "@/components/ui/button"
 import { Menu } from "lucide-react"
 import Link from "next/link"
 
-// FIXME: use next/link instead of a
-
 function Header() {
   return (
     <div className="border-b fixed top-0 inset-x-0 bg-white z-10">
       <div className="flex justify-between h-16 items-center px-4 max-w-7xl mx-auto">
-        <a href="/" className="font-extrabold mr-5">MSK</a>
+        <Link href="/" className="font-extrabold mr-5">MSK</Link>
         <div className="w-full items-center hidden sm:flex">
           <MainNav className="mx-6" />
           <Social />
--- src/components/layout/header/social.tsx
+++ src/components/layout/header/social.tsx
@@ -1,39 +1,38 @@
 import { cn } from "@/lib/utils"
 import { Github, LinkedinIcon, TwitterIcon, Instagram } from "lucide-react"
+import Link from "next/link"
 
 function Social() {
-  // FIXME: use next/link instead of a
-
   return (
     <div className="ml-auto flex items-center space-x-4">
-      <a
+      <Link
         href="https://github.com/sefakpsz"
         className="text-zinc-500 transition-colors hover:text-primary"
         target="_blank"
       >
         <Github height={20} />
-      </a>
-      <a
+      </Link>
+      <Link
         href="https://linkedin.com/in/sefakpsz"
         className="text-zinc-500 transition-colors hover:text-primary"
         target="_blank"
       >
         <LinkedinIcon height={20} />
-      </a>
-      <a
+      </Link>
+      <Link
         href="https://twitter.com/sefakpsz"
         className="text-zinc-500 transition-colors hover:text-primary"
         target="_blank"
       >
         <TwitterIcon height={20} />
-      </a>
-      <a
+      </Link>
+      <Link
         href="https://instagram.com/sefakpsz"
         className="text-zinc-500 transition-colors hover:text-primary"
         target="_blank"
       >
         <Instagram height={20} />
-      </a>
+      </Link>
     </div>
   )
 }
